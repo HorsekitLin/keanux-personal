87d2951fda59c0befee08ac2ac53d10d8cbc9ae5
Tomas <horsekit1982@gmail.com>
2015-05-28 08:28:36 +0800

    initial kenux version
    第一次抓下來的版本

--- public/scripts/posts.js
+++ public/scripts/posts.js
@@ -12,15 +12,15 @@ var Post = React.createClass({displayName: "Post",
     var rawMarkup = converter.makeHtml(this.props.children.toString());
     var icon = "https://graph.facebook.com/" + this.props.username + "/picture?width=120&height=120";
     return (
-      React.createElement("div", {className: "post"}, 
-        React.createElement(PostMeta, {username: this.props.username, nickname: this.props.nickname, create_time: this.props.create_time}), 
-        React.createElement("h2", {className: "postTitle"}, 
+      React.createElement("div", {className: "post"},
+        React.createElement(PostMeta, {username: this.props.username, nickname: this.props.nickname, create_time: this.props.create_time}),
+        React.createElement("h2", {className: "postTitle"},
           React.createElement("a", {href: "http://keanux.com/p/" + this.props.unique_id}, this.props.title)
-        ), 
-        React.createElement("h3", {className: "postSubTitle"}, 
+        ),
+        React.createElement("h3", {className: "postSubTitle"},
           this.props.subtitle
-        ), 
-        React.createElement("div", {className: "postContent"}, 
+        ),
+        React.createElement("div", {className: "postContent"},
           strip(rawMarkup)
         )
       )
@@ -33,16 +33,16 @@ var PostMeta = React.createClass({displayName: "PostMeta",
   render: function() {
     var then = new Date(this.props.create_time * 1000);
     return (
-      React.createElement("div", {className: "block-postMeta postMeta-previewHeader"}, 
-        React.createElement("div", {className: "u-floatLeft"}, 
-          React.createElement("div", {className: "postMetaInline-avatar"}, 
-            React.createElement("a", {href: "http://keanux.com/@" + this.props.username}, 
+      React.createElement("div", {className: "block-postMeta postMeta-previewHeader"},
+        React.createElement("div", {className: "u-floatLeft"},
+          React.createElement("div", {className: "postMetaInline-avatar"},
+            React.createElement("a", {href: "http://keanux.com/@" + this.props.username},
               React.createElement("img", {src: 'https://graph.facebook.com/' + this.props.username + '/picture?width=120&height=120', className: "avatar-image avatar-image--smaller"})
             )
-          ), 
-          React.createElement("div", {className: "postMetaInline-feedSummary"}, 
-            React.createElement("a", {href: "http://keanux.com/@" + this.props.username}, this.props.nickname), 
-            React.createElement("span", {className: "postMetaInline postMetaInline--supplemental"}, 
+          ),
+          React.createElement("div", {className: "postMetaInline-feedSummary"},
+            React.createElement("a", {href: "http://keanux.com/@" + this.props.username}, this.props.nickname),
+            React.createElement("span", {className: "postMetaInline postMetaInline--supplemental"},
               React.createElement(Ago, {date: then, autoUpdate: true, tooltipFormat: "long"})
             )
           )
@@ -70,8 +70,8 @@ var PostBox = React.createClass({displayName: "PostBox",
   },
   render: function() {
     return (
-      React.createElement("div", {className: "postBox"}, 
-        React.createElement("h1", null, "Keanux"), 
+      React.createElement("div", {className: "postBox"},
+        React.createElement("h1", null, "Keanux"),
         React.createElement(PostList, {data: this.state.data})
       )
     );
@@ -85,13 +85,13 @@ var PostList = React.createClass({displayName: "PostList",
         // `key` is a React-specific concept and is not mandatory for the
         // purpose of this tutorial. if you're curious, see more here:
         // http://facebook.github.io/react/docs/multiple-components.html#dynamic-children
-        React.createElement(Post, {unique_id: post.unique_id, nickname: post.nickname, title: post.title, subtitle: post.subtitle, username: post.username, create_time: post.create_time, key: index}, 
+        React.createElement(Post, {unique_id: post.unique_id, nickname: post.nickname, title: post.title, subtitle: post.subtitle, username: post.username, create_time: post.create_time, key: index},
           post.content
         )
       );
     });
     return (
-      React.createElement("div", {className: "postList"}, 
+      React.createElement("div", {className: "postList"},
         postNodes
       )
     );
